7f047166df2b66dcb58e9e555b7e4349e45fce05
Lux Aliaga <lux@nixgoat.me>
2024-05-13 03:33:11 -0400

    main: Default logging
    There'll be an option to disable this, but for now it's there!

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,23 @@ mod api;
 mod proxy;
 mod scraping;
 mod utils;
-use std::collections::HashMap;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::{Compat, Logger}, web, App, HttpServer};
 use proxy::KeyStore;
+use std::collections::HashMap;
 use tokio::sync::Mutex;
+use tracing_actix_web::TracingLogger;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    tracing_subscriber::fmt::init();
     let db = web::Data::new(KeyStore {
         content: Mutex::new(HashMap::new()),
     });
     HttpServer::new(move || {
         App::new()
+            .wrap(Compat::new(TracingLogger::default()))
+	    .wrap(Logger::default())
             .service(web::scope("/api/v1").service(api::post).service(api::user))
             .service(web::scope("/proxy").service(proxy::proxy))
             .app_data(db.clone())
